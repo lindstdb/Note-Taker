3f0a0a00288b95e7fb968d1987e7a537be271642
lindstdb <115056565+lindstdb@users.noreply.github.com>
2023-02-14 18:14:01 -0600

    switched the routes to .Router in apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,32 +1,32 @@
-const fs = require('fs');
-const path = require('path');
+const router = require('express').Router();
+const store = require('../db/store');
 
-var uniqid = require('uniqid');
+router.get('/notes', (req, res) => {
+    store
+        .getNotes()
+        .then(notes => {
+            res.json(notes)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        });
+});
 
-module.exports = (app) => {
-    app.get('/api/notes', (req, res) => {
-        res.sendFile(path.join(__dirname, '../db/db.json'));
-    });
+router.post('/notes', (req, res) => {
+    console.log(req.body)
+    store
+        .addNote(req.body)
+        .then(note => {
+            res.json(note)
+        })
+        .catch(err => {
+             res.status(500).json(err)
+        });
+});
 
-    app.post('/api/notes', (req, res) => {
-        let db = fs.readFileSync('db/db.json');
-        db = JSON.parse(db);
-        res.json(db);
-
-        let userNote = {
-            title: req.body.title,
-            text: req.body.text,
-            id: uniqid(),
-        };
-
-        db.push(userNote);
-        fs.writeFileSync('db/db.json', JSON.stringify(db));
-        res,json(db);
-    });
-
-    app.delete('/api/notes/:id', (req, res) => {
-        let db = JSON.parse(fs.readFileSync('db/db.json'));
-        fs.writeFileSync('db/db.json', JSON.stringify(deleteNotes));
-        res.json(deleteNotes);
-    })
-};
+router.delete('/notes/:id', (req, res) => {
+    store
+        .removeNote(req.params.id)
+        .then(() => res.json({ ok: true }))
+        .catch(err => res.status(500).json(err));
+});
--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoute = require('./routes/htmlRoute');
+
 
 app.use(express.static('public'));
 app.use(express.rrlencoded({ extended: true }));
 app.use(express.json());
-
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoute')(app);
+app.use('/api', apiRoutes);
+app.use('/', htmlRoute);
 
 app.listen(PORT, () => {
-    console.log(`Server available at localHost${PORT}`);
+    console.log('Server available at port:${PORT}')
 });
